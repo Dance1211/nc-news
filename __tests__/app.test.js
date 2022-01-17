271897026108179e17350c5b064f940ef76abc53
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-01-17 16:27:11 +0000

    Added testing for non-existant endpoint errors

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,6 +7,25 @@ const app = require('../app');
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
+describe('/api/notAnEndpoint', () => {
+  test('Return a 404 for a non-existant endpoint', () => {
+    return request(app)
+      .get('/api/notAnEndpoint')
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Endpoint '/api/notAnEndpoint' does not exist")
+      })
+  });
+  test('Ignores the query part of the URL in the response', () => {
+    return request(app)
+      .get('/api/notAnEndpoint?isIgnored=true')
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Endpoint '/api/notAnEndpoint' does not exist")
+      })
+  });
+});
+
 describe('/api', () => {
   describe('GET', () => {
     test('Returns an object describing the various endpoints', () => {
--- middleware/errors.js
+++ middleware/errors.js
@@ -2,6 +2,6 @@ module.exports.serverError = (err, req, res, next) => {
   res.status(500).send({msg: "Internal server error"});
 }
 
-module.exports.invalidEndpointError = (req, res, next) => {
-  res.status(404).send({msg: "Endpoint does not exist"});
+module.exports.invalidEndpointError = (req, res) => {
+  res.status(404).send({msg: `Endpoint '${req.path}' does not exist`});
 }
