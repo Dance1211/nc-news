74c7558f865ecf68762b901a52d9b3ef7bfd52a1
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-01-17 17:06:54 +0000

    Fixed topics model typo

--- models/topics.model.js
+++ models/topics.model.js
@@ -3,8 +3,8 @@ const db = require("../db/connection");
 module.exports.selectTopics = async () => {
   try {
     const topics = await db.query(`
-    SELECT * FROM topics;
-  `);
+      SELECT * FROM topics;
+    `);
     return topics.rows;
   } catch (error) {
     return Promise.reject(error);
