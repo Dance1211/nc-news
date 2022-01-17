2c99e0f5f2925625e482907101f78a53c3a4d486
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-01-17 16:16:26 +0000

    Added basic error handling middleware to app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const apiRouter = require("./routes/api.routes");
 const app = express();
+const errors = require('./middleware/errors');
 
 app.use(express.json());
 
 app.use('/api', apiRouter);
 
-app.use((err, req, res, next) => {
-  res.status(500).send(err);
-})
+
+// Handle errors
+app.use(errors.serverError);
+
+// Handle general 404's
+app.use(errors.invalidEndpointError);
 
 module.exports = app;
