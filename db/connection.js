19ce976bb7dc1ab40bf764c59dd6124a69c4eff9
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-01-18 22:49:48 +0000

    Configured the server ready for heroku

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,25 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+// Know which database we're using depending on if we're in a testing enviroment or not
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+// Error handling for compulsary enviromental constants
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+// Configure the connection depending ont he enviroment
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
+module.exports = new Pool(config);
